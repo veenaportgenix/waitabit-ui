0803958c297f91b9595d8401ee77bb61f8c4c76d
Devi <devi@blocksurvey.org>
2020-09-13 12:40:40 +0530

    More changes related to UI, dynamic table

--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tables.component.scss']
 })
 export class TablesComponent implements OnInit {
-
+  waitListData = [];
+  data:any 
   constructor() { }
 
   ngOnInit() {
+    let value  = 100  
+    let a = []; 
+    for(let i = 1; i < value; ++i) { 
+      this.data={
+        "priority": i,
+        "referral_link": "https://cryptoally.io/?&ref_id=AA7T4",
+        "registered_email": "bani@getwaitlist.com",
+        "total_waiters_currently": i - 2,
+        "createdAt":"23/4/2020",
+        "noReferred": 2
+        }
+        this.waitListData.push(this.data)
+    } 
   }
-
 }
